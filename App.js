5c024c5c7cbcf6a277102d31314c00b212e0c0a1
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-08-08 11:28:23 -0500

    listview added

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container} />
+        <View style={{ flex: 1}}>
+          <Header headerText="Tech Stack" /> 
+          <LibraryList /> 
+        </View>
       </Provider>
       
     );
